b5040d433fdbb2768d43c84f74d06094679b1bfc
tzlloch@gmail.com <tzlloch@gmail.com@0441bf06-048d-d659-6b34-d1af66c0f4fa>
2011-10-10 08:59:47 +0000

    update alignhash ut

--- test/test_alignhash.cpp
+++ test/test_alignhash.cpp
@@ -1,42 +1,45 @@
 #include <stdio.h>
 #include <assert.h>
+#include <string.h>
+#include <ulib/hash.h>
 #include <ulib/alignhash.h>
 
-struct key_struct
-{
-	const unsigned char *str;
+struct str {
+	const char *c_str;
+	
+	str(const char *s = 0)
+	: c_str(s) { }
 
-	operator uint64_t() const
-	{
-		uint64_t hash = 0;
+	operator size_t() const
+	{ return hash_djb2((const unsigned char *)c_str); }
 
-		for (const unsigned char *p = str; *p; ++p)
-			hash += hash * 33 + *p;
-
-		return hash;
-	}
+	bool operator==(const str &other) const
+	{ return strcmp(c_str, other.c_str) == 0; }
 };
 
 int main()
 {
-	align_hash_map<int,int> map;
-
-	map[1] = 100;
-	map[1000] = 101;
-
-	assert(100 == map[1]);
-	assert(101 == map[1000]);
-
-	align_hash_map<key_struct, int> keymap;
-
-	key_struct key;
-
-	key.str = (const unsigned char *)"hello world";
-	keymap[key] = 999;
-
-	assert(999 == keymap[key]);
+	align_hash_map<str, int> months;
+  
+	months["january"] = 31;
+	months["february"] = 28;
+	months["march"] = 31;
+	months["april"] = 30;
+	months["may"] = 31;
+	months["june"] = 30;
+	months["july"] = 31;
+	months["august"] = 31;
+	months["september"] = 30;
+	months["october"] = 31;
+	months["november"] = 30;
+	months["december"] = 31;
+  
+	assert(months["september"] == 30);
+	assert(months["april"] == 30);
+	assert(months["february"] == 28);
+	assert(months["december"] == 31);
 
 	printf("passed\n");
-	
+
 	return 0;
 }
